9375aec76ceb2e0c56646c233baf9469b9f399c6
Aemon Fatima <aemonfatimaimtiaz300262@gmail.com>
2024-07-10 12:06:06 +0500

    updated

---
+++ src/components/Login.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Button, Input, Form, message, Typography, Card } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { login } from '../store/actions/authActions';
+
+const { Title } = Typography;
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = useSelector((state) => state.auth);
+
+  const handleSubmit = () => {
+    dispatch(login(username, password));
+  };
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate('/Landing');
+    } else if (auth.error) {
+      message.error(auth.error);
+    }
+  }, [auth, navigate]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Card style={{ width: 400, padding: '20px', textAlign: 'center' }}>
+        <Title level={3}>Login</Title>
+        <Form
+          name="loginForm"
+          initialValues={{ remember: true }}
+          onFinish={handleSubmit}
+          style={{ marginTop: '20px' }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
